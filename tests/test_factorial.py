21fa0cfae084f5db3a1e22a4c612b48a29440585
fronre <islamhala35@gmail.com>
2026-02-10 23:49:02 +0100

    red: add test for factorial(1)

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -3,3 +3,6 @@ def test_factorial_is_callable():
       factorial()
 def test_factorial_zero_is_one():
     assert factorial(0) == 1
+
+def test_factorial_one_is_one():
+    assert factorial(1) == 1
