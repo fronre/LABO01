475974ba9a9a76c0ef24773022762948a36c1de3
fronre <islamhala35@gmail.com>
2026-02-11 00:19:57 +0100

    blue: refactor factorial tests using parametrize

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -6,12 +6,21 @@ from factorial import factorial
     [
         (0, 1),
         (1, 1),
+
         (2, 2),
         (3, 6),
         (4, 24),
-       (  5, 120)
+        (5, 120),
 
+        (6, 720),
+        (7, 5040),
+        (8, 40320),
+        (9, 362880),
+        (10, 3628800),
+
+        (12, 479001600),
     ]
 )
 def test_factorial_values(n, expected):
     assert factorial(n) == expected
+
