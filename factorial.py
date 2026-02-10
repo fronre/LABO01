070473300b1c9601eac5edf4fca699b0a95eaac9
fronre <islamhala35@gmail.com>
2026-02-11 00:11:42 +0100

    blue:refactoring

--- factorial.py
+++ factorial.py
@@ -1,10 +1,4 @@
 def factorial(n: int) -> int:
       if n == 0 or n == 1:
             return 1
-      if n==2:
-            return 2
-      if n==3:
-            return 6
-      if n==4:
-            return 24
-      return 120
+      return n * factorial(n - 1)
